c298db0c8c1381a64a60aa11cd5c3a3209540eea
Vector A <Vector5233@gmail.com>
2025-02-14 09:39:27 -0500

    We programed the different functions of the robot Arm-Started

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LEARNING_TO_PROGRAM.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+//@Disabled
+@TeleOp(name="learning",group="robotics club")
+public class LEARNING_TO_PROGRAM extends LinearOpMode {
+
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+    double Left;
+    double Right;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initializedRobot();
+      waitForStart();
+
+      while (opModeIsActive()) {
+          moveForward();
+          turnLeft();
+          moveBackward();
+        }
+      }
+
+
+
+    private void moveBackward() {
+    }
+
+    private void turnLeft() {
+    }
+
+    private void moveForward() {
+        Left=gamepad1.left_stick_y;
+        Right=gamepad1.right_stick_y;
+        leftMotor.setPower(Left);
+        rightMotor.setPower(Right);
+
+    }
+
+    private void initializedRobot() {
+        leftMotor= hardwareMap.get(DcMotor.class,"lw");
+        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightMotor= hardwareMap.get(DcMotor.class,"rw");
+        rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+     }
+    }
+
