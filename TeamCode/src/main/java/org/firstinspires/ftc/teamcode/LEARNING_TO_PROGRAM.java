37f38470ffd85d6072866332b165137fe0943480
Vector A <Vector5233@gmail.com>
2025-02-20 15:10:03 -0500

    We programed the different functions of the robot Arm-Started Forward and Backward Tank Movement with joystics added.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LEARNING_TO_PROGRAM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LEARNING_TO_PROGRAM.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
+// updated
 //@Disabled
 @TeleOp(name="learning",group="robotics club")
 public class LEARNING_TO_PROGRAM extends LinearOpMode {
